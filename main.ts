74ebff7152c80d2e809d3363977c427c6e24cad1
Joey J <joey@wyxos.com>
2024-12-24 11:47:25 +0400

    chore: alignments

--- main.ts
+++ main.ts
@@ -16,37 +16,38 @@ async function scaffoldLaravelPackage(packageName: string) {
   await ensureDir(`${baseDir}/src`);
   await ensureDir(`${baseDir}/src/Providers`);
 
-  let templateBase: string;
-
-  if (import.meta.url.startsWith("file://")) {
-    // Local execution
-    templateBase = fromFileUrl(new URL("./templates/", import.meta.url));
-  } else if (import.meta.url.startsWith("https://")) {
-    // Global execution
-    templateBase = new URL("./templates/", import.meta.url).toString();
-  } else {
-    throw new Error("Unsupported execution context.");
+  async function readTemplate(templatePath: string): Promise<string> {
+    if (templatePath.startsWith("file://")) {
+      // Local execution
+      return await Deno.readTextFile(new URL(templatePath));
+    } else if (templatePath.startsWith("https://")) {
+      // Global execution
+      const response = await fetch(templatePath);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch template: ${response.statusText}`);
+      }
+      return await response.text();
+    } else {
+      throw new Error("Unsupported template path.");
+    }
   }
 
-  const composerTemplate = await Deno.readTextFile(
-    `${templateBase}composer.template.json`,
-  );
+  const templateBase = new URL("./templates/", import.meta.url).toString();
+
+  const composerTemplate = await readTemplate(`${templateBase}composer.template.json`);
   const composerJson = composerTemplate
-    .replace(/{{PACKAGE_NAME}}/g, packageName)
-    .replace(/{{NAMESPACE}}/g, namespace)
-    .replace(/{{CLASS_NAME}}/g, className);
+      .replace(/{{PACKAGE_NAME}}/g, packageName)
+      .replace(/{{NAMESPACE}}/g, namespace)
+      .replace(/{{CLASS_NAME}}/g, className);
   await Deno.writeTextFile(`${baseDir}/composer.json`, composerJson);
 
-  // Step 3: Read and Populate Service Provider Template
-  const serviceProviderTemplate = await Deno.readTextFile(
-    `${templateBase}ServiceProviderTemplate.php`,
-  );
+  const serviceProviderTemplate = await readTemplate(`${templateBase}ServiceProviderTemplate.php`);
   const serviceProvider = serviceProviderTemplate
-    .replace(/{{NAMESPACE}}/g, namespace)
-    .replace(/{{CLASS_NAME}}/g, className);
+      .replace(/{{NAMESPACE}}/g, namespace)
+      .replace(/{{CLASS_NAME}}/g, className);
   await Deno.writeTextFile(
-    `${baseDir}/src/Providers/${className}.php`,
-    serviceProvider,
+      `${baseDir}/src/Providers/${className}.php`,
+      serviceProvider,
   );
 
   // Step 4: Create .gitignore File
